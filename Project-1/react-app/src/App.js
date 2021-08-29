a76d86a6a89487a7aaad98decdde95d6035c6431
SecureNeo <secureneophyte@gmail.com>
2021-08-29 21:11:38 +0530

    Initial Commit

---
+++ Project-1/react-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/*
+          Edit <code>src/App.js</code> and save to reload.
+          */}
+          Welcome to the new Era of Azure DevOps Integration and Releases!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kick Starting with REACT Framework
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
